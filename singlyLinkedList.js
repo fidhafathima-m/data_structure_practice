e94ce0cb8dd9b3d7396b928d4286daf02831e9cf
Fidha Fathima M <fidhumusthafa3549@gmail.com>
2025-03-13 11:19:30 +0530

    practiced search in SSL

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -56,6 +56,56 @@ class LinkedList {
         }
     }
 
+    removeIndex(index) {
+        if (index < 0 || index >= this.size) return;
+        let removedNode;
+        if(index === 0) {
+            removedNode = this.head;
+            this.head = this.head.next;
+        } else {
+            let prev = this.head;
+            for(let i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            removedNode = prev.next;
+            prev.next = removedNode.next;
+        }
+        this.size--;
+        return removedNode.val;
+    }
+
+    removeValue(val) {
+        if(this.isEmpty()) return null;
+        if(this.head.val === val) {
+            this.head = this.head.next;
+            this.size--;
+            return val;
+        } else {
+            let prev = this.head;
+            while(prev.next && prev.next.val !== val) {
+                prev = prev.next;
+            }
+            let removedNode = prev.next;
+            prev.next = removedNode.next;
+            this.size--;
+            return val;
+        }
+    }
+
+    search(val) {
+        if(this.isEmpty()) return -1;
+        let curr = this.head;
+        let i = 0;
+        while(curr) {
+            if(curr.val === val) {
+                return i;
+            }
+            curr = curr.next;
+            i++;
+        }
+        return -1;
+    }
+
     print() {
         if (this.size === 0) {
             console.log("List is empty");
@@ -95,9 +145,14 @@ list.insertFront(100);
 list.insertFront(200);
 list.insertFront(300);
 list.print(); // 300 -> 200 -> 100
-list.reverse();
-list.print(); // 100 -> 200 -> 300
 list.insertEnd(500);
 list.print(); // 100 -> 200 -> 300 -> 500
 list.insertAtIndex(600, 2);
 list.print(); // 100 -> 200 -> 600 -> 300 -> 500
+list.removeIndex(0);
+list.print();
+list.removeValue(600);
+list.print();
+list.reverse();
+list.print(); 
+console.log(list.search(100));
